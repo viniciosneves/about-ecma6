7a8327da23a9977bc7fd1ee6a0ca6f12b34ca98f
= <=>
2017-02-13 21:37:18 -0200

    encapsulando a classe

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -2,13 +2,29 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
 
-    obterVolume() {
+    get volume() {
 
         return this.quantidade * this.valor;
     }
+
+    get data() {
+
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    get valor() {
+
+        return this._valor;
+    }        
 }
