62c8b6de1610246c262ad52dded7317a6ddf12f3
= <=>
2017-02-15 20:53:55 -0200

    lista de negociacoes + programacao defensiva

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -5,9 +5,10 @@ class NegociacaoController {
         
         let $ = document.querySelector.bind(document);
 
-        this._inputData       = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor      = $('#valor');
+        this._inputData             = $('#data');
+        this._inputQuantidade       = $('#quantidade');
+        this._inputValor            = $('#valor');
+        this._listaNegociacoes      = new ListaNegociacoes();
 
     }
 
@@ -15,18 +16,24 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...
-                this._inputData.value.split('-')
-                .map((item, indice) => indice == 1 ? item -1 : item)
-            );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
-            data,
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        );
+        )
+    }
 
-        console.log(negociacao);
+    _limpaFormulario() {
 
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
